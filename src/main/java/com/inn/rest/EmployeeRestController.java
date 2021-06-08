1d680ac177cb63b1a2687c4087610a1026582f30
unknown <sagardahibhate1@gmail.com>
2021-06-08 12:19:45 -0700

    initial commit

---
+++ src/main/java/com/inn/rest/EmployeeRestController.java
@@ -0,0 +1,16 @@
+package com.inn.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/producer")
+public class EmployeeRestController {
+
+	@GetMapping("/show")
+	public ResponseEntity<String> showMsg() {
+		return ResponseEntity.ok("FROM EMPLOYEE SERVICE!!");
+	}
+}
